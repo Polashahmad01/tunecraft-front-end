7c549d52e7bdc94559cc37e64659cbdc0b73f911
Polashahmad01 <dev.polashahmad@gmail.com>
2024-03-26 21:34:16 +0600

    Update page titles and styles for login and error pages

--- src/views/ErrorPage.jsx
+++ src/views/ErrorPage.jsx
@@ -9,7 +9,7 @@ export default function ErrorPage() {
   }
 
   return (
-    <section className="bg-white dark:bg-gray-900 ">
+    <section className="bg-white dark:bg-gray-900">
       <div className="container flex items-center min-h-screen px-6 py-12 mx-auto">
         <div className="flex flex-col items-center max-w-sm mx-auto text-center">
           <p className="p-3 text-sm font-medium text-blue-500 rounded-full bg-blue-50 dark:bg-gray-800">
--- src/views/LoginPage.jsx
+++ src/views/LoginPage.jsx
@@ -3,7 +3,7 @@ export default function LoginPage() {
   
   return (
     <section>
-      <h1>Login Page</h1>
+      <h1 className="text-3xl font-bold">Login Page</h1>
     </section>
   )
 }
