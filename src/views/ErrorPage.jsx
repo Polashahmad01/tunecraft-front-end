8aa7cce5a7fb200773edb9dc70c19b890645afd7
Polashahmad01 <dev.polashahmad@gmail.com>
2024-03-27 04:46:19 +0600

    Update background color and button style in ErrorPageview

--- src/views/ErrorPage.jsx
+++ src/views/ErrorPage.jsx
@@ -9,7 +9,7 @@ export default function ErrorPage() {
   }
 
   return (
-    <section className="bg-white dark:bg-gray-900">
+    <section className="bg-white dark:bg-black">
       <div className="container flex items-center min-h-screen px-6 py-12 mx-auto">
         <div className="flex flex-col items-center max-w-sm mx-auto text-center">
           <p className="p-3 text-sm font-medium text-blue-500 rounded-full bg-blue-50 dark:bg-gray-800">
@@ -26,7 +26,7 @@ export default function ErrorPage() {
               </svg>
               <Link onClick={goBackHandler}>Go back</Link>
             </button>
-            <button className="w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600">
+            <button className="w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 border rounded-lg shrink-0 sm:w-auto hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600 dark:text-gray-200">
               <Link to="/">
                 Home Page
               </Link>
