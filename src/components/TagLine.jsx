7bda7d39f8b0c213c02032502a545e4b3adef638
Polashahmad01 <dev.polashahmad@gmail.com>
2024-03-27 05:44:08 +0600

    Add Typewriter effect to TagLine component

---
+++ src/components/TagLine.jsx
@@ -0,0 +1,25 @@
+import Typewriter from "typewriter-effect";
+import musicIcon from "../assets/images/music-icon.png";
+
+export default function TagLine() {
+  const options = {
+    strings: ['Rock Music', 'Pop Music', 'Hip-Hop Music', 'Rap Music'],
+    autoStart: true,
+    loop: true
+  }
+
+  return (
+    <div className="bg-[#0A0A0A]/75 dark:bg-black text-white grid grid-rows-1 place-items-center h-full px-4">
+      <div className="text-center lg:text-left">
+        <h1 className="text-2xl font-semibold sm:mb-4 text-white dark:text-white md:text-4xl">Create Awesome</h1>
+        <h1 className="text-2xl font-semibold mb-2 sm:mb-4 text-white dark:text-white md:text-4xl">
+          <Typewriter options={options} />
+        </h1>
+        <div className="mb-2">
+          <img className="w-1/2 m-auto lg:w-auto lg:m-0" src={musicIcon} alt="TuneCraft Music Icon" />
+        </div>
+        <h1 className="text-2xl font-semibold text-white dark:text-white md:text-4xl">In Less Than Few Minutes</h1>
+      </div>
+    </div>
+  )
+}
--- src/views/LoginPage.jsx
+++ src/views/LoginPage.jsx
@@ -1,9 +1,15 @@
+import TagLine from "../components/TagLine";
 
 export default function LoginPage() {
   
   return (
-    <section>
-      <h1 className="text-3xl font-bold">Login Page</h1>
+    <section className="bg-white dark:bg-black">
+      <div className="grid grid-cols-1 min-h-screen lg:grid-cols-2">
+        <div className="text-3xl font-bold text-black dark:text-white">Login Form</div>
+        <div className="">
+          <TagLine />
+        </div>
+      </div>
     </section>
   )
 }
