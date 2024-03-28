9549f53b9477691563166ef25bd5b0fb439bfd2e
Polashahmad01 <dev.polashahmad@gmail.com>
2024-03-28 21:36:40 +0600

    Add RegisterForm component to RegisterPage view

--- src/views/RegisterPage.jsx
+++ src/views/RegisterPage.jsx
@@ -1,9 +1,18 @@
+import TagLine from "../components/TagLine";
+import RegisterForm from "../components/RegisterForm";
 
 export default function RegisterPage() {
 
   return (
-    <section>
-      <h1>Register Page</h1>
+    <section className="bg-white dark:bg-black">
+      <div className="grid grid-cols-1 min-h-screen lg:grid-cols-2">
+      <div className="grid grid-cols-1 place-items-center">
+        <RegisterForm />
+      </div>
+      <div className="">
+        <TagLine />
+      </div>
+      </div>
     </section>
   )
 }
